8f802738abc00b5b387442119be4681948e25389
Loic Stas <lolostas@hotmail.com>
2017-08-01 17:31:33 +0200

    articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.destroy_all
+
+Article.create([
+  { title: 'Les maladies mortelles', content: 'Les maladies mortelles tuent !!!!!!!!' },
+  { title: 'Walah y a chaleur', content: 'la canicule va-t-elle tuer + que jamais ?' },
+])
+
+
+
+50.times do
+  Article.create(
+    title: Faker::LordOfTheRings.character,
+    content: Faker::Pokemon.move
+  )
+end
